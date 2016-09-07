9b765c4c69033c45e8b074e704b85f73f1c69514
gophic <gophic@icloud.com>
2016-09-07 10:18:05 +0800

    add the url that include details, result and vote. set up their view.

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^P(?P<question_id>[0-9]+)/$', views.detail,name='details'),
+    url(r'^P(?P<question_id>[0-9]+)/result/$', views.result, name='result'),
+    url(r'^P(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse("hello world.You are at the polls index.")
+
+def index(request, question_id):
+    return HttpResponse("hello world.You are at the polls %s." % question_id)
+
+
+def result(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def detail(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)

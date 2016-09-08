a34e5968eed197e65bb00c09cfdb77f93fd81455
gophic <gophic@icloud.com>
2016-09-08 08:36:20 +0800

    create view template , and update some url and views model.

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name="polls"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^P(?P<question_id>[0-9]+)/$', views.detail,name='details'),
-    url(r'^P(?P<question_id>[0-9]+)/result/$', views.result, name='result'),
-    url(r'^P(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/result/$', views.result, name='result'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
 # Create your views here.
+from .models import Question
 
 
-def index(request, question_id):
-    return HttpResponse("hello world.You are at the polls %s." % question_id)
+def index(request):
+    last_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template("polls/index.html")
+#    output= ', '.join([q.question_text for q in last_question])
+    context = {
+        "last_question_list": last_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def result(request, question_id):
@@ -13,8 +22,14 @@ def result(request, question_id):
 
 
 def detail(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question Dose not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+#    response = "You're looking at the results of question %s."
+#    return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):

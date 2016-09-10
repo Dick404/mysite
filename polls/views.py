7542af5ed31505bc4d26cba28bf1c4fa211c65bb
gophic <gophic@icloud.com>
2016-09-10 14:47:02 +0800

    add views of result template, test the function.

--- polls/views.py
+++ polls/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 # Create your views here.
 from .models import Question, Choice
 
+'''
 
 def index(request):
     last_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -47,4 +49,39 @@ def vote(request, question_id):
         select_choice.votes += 1
         select_choice.save()
 #    response = "You're looking at the results of question %s."
+    return HttpResponseRedirect(reverse("polls:result", args=(question_id,)))
+'''
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'last_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        select_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            'question': question,
+            "error_message": "You didn't select a Choice",
+        })
+    else:
+        select_choice.votes += 1
+        select_choice.save()
+        #    response = "You're looking at the results of question %s."
     return HttpResponseRedirect(reverse("polls:result", args=(question_id,)))
